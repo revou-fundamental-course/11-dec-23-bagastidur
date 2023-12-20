5eae4d5740c0be78438629594ea599be6eb8453e
bagastidur <bagasagung4@gmail.com>
2023-12-20 22:38:32 +0700

    feat: add banner autosliding, form validation, and navbar navigation

--- js/script.js
+++ js/script.js
@@ -6,3 +6,28 @@ window.onload = function() {
     targetWord.textContent = newWord;
   }
 };
+
+const nameInput = document.getElementById("name")
+const dobInput = document.getElementById("dob")
+const submitButton = document.getElementById("submit")
+
+nameInput.addEventListener("keyup", function(event){
+    var fullName = nameInput.value
+    if (fullName == "") {
+        submitButton.disabled = true
+    } else {
+        submitButton.disabled = false
+    }
+});
+
+let currentIndex = 0
+const images = document.querySelectorAll(".sliding-image")
+const totalImages = images.length 
+
+function changeImage() {
+    images[currentIndex].style.display = "none"
+    currentIndex = (currentIndex + 1) % totalImages
+    images[currentIndex].style.display = "block"
+}
+
+setInterval(changeImage, 2000)
