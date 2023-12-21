b00234fe6c2567dd7a9fb52ef4b61e3783fd6925
bagastidur <bagasagung4@gmail.com>
2023-12-21 19:02:45 +0700

    feat: add form validation on messageus form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const targetWord = document.getElementById("targetWord");
 
-window.onload = function() {
+window.onload = function () {
   const newWord = prompt("What is your name?");
   if (newWord) {
     targetWord.textContent = newWord;
@@ -9,12 +9,12 @@ window.onload = function() {
 
 let currentIndex = 0
 const images = document.querySelectorAll(".sliding-image")
-const totalImages = images.length 
+const totalImages = images.length
 
 function changeImage() {
-    images[currentIndex].style.display = "none"
-    currentIndex = (currentIndex + 1) % totalImages
-    images[currentIndex].style.display = "block"
+  images[currentIndex].style.display = "none"
+  currentIndex = (currentIndex + 1) % totalImages
+  images[currentIndex].style.display = "block"
 }
 
 setInterval(changeImage, 2000);
@@ -23,9 +23,25 @@ const nameInput = document.getElementById("name")
 const submitButton = document.getElementById("submit")
 
 nameInput.addEventListener("input", () => {
-    if (nameInput.value.trim() !== "") {
-      submitButton.disabled = false;
-    } else {
-      submitButton.disabled = true;
-    }
-  });
+  if (nameInput.value.trim() !== "") {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
+
+function validateForm() {
+  const textarea = document.getElementById("message");
+  const textLength = textarea.value.trim().length;
+
+  if (textLength < 10) {
+    alert("Please enter at least 10 characters in the textarea.");
+    textarea.focus(); 
+    return false; 
+  }
+
+  return true; 
+}
+
+const form = document.getElementById("form-section");
+form.addEventListener("submit", validateForm);
